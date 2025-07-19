325376680e6fb340a3a0b6952126bc9ac4a2ce7a
gunitaa <gunita1576.be23@chitkara.edu.in>
2025-07-19 16:14:58 +0530

    assignment Saturday

---
+++ MyAssigment/MyAssigmentTests/MyAssigmentTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyAssigmentTests.swift
+//  MyAssigmentTests
+//
+//  Created by Student on 19/07/25.
+//
+
+import Testing
+@testable import MyAssigment
+
+struct MyAssigmentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
